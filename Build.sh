54f254fb960a2c97acd49aad3693bd27f2669ab7
Hoang Dang Khoa <khoahoang@cqtdt.asia>
2023-05-10 22:23:25 +0700

    khoa

---
+++ Build.sh
@@ -0,0 +1,7 @@
+DOCKER_BUILDKIT=1 docker build -f /root/khoa/Dockerfile -t khoa0797/nginx-ubuntu:latest .
+
+docker build -t khoa0797/nginx-ubuntu:latest -t khoa0797/nginx-ubuntu:1.0.0 .
+
+docker login -u "khoa0797" -p "dangkhoabro9x" docker.io
+
+docker push khoa0797/nginx-ubuntu:latest
